16c260a26410c4bc1b7c4f557f0c0924a3026947
Ignacy Moryc <imoryc@gmail.com>
2013-01-11 17:14:48 +0100

    Better tests names

--- test/woda/test/core.clj
+++ test/woda/test/core.clj
@@ -1,29 +1,35 @@
 (ns woda.test.core
+  (:import [com.gargoylesoftware.htmlunit ElementNotFoundException])
   (:use woda.test.support)
   (:use [woda.core])
   (:use midje.sweet))
 
 (against-background
  [(before :facts (setup-test-server) :after (stop-test-server))]
- (fact
-  (-> (visit "http://localhost:8008")
-      (page-has? "Hello")) => truthy)
- (fact
-  (-> (visit "http://localhost:8008")
-      (page-has? "Unicorn")) => falsey)
- (fact
-  (-> (visit "http://localhost:8008")
-      (page-get-element-by-id "subtitle")
-      (content)) => "This is a subtitle")
 
- (fact
-  (-> (visit "http://localhost:8008")
-      (click-link "Follow me!")
-      (page-has? "I'm on a second page!")) => truthy)
+ (facts "about checking page content"
+        (-> (visit "http://localhost:8008")
+            (page-has? "Hello")) => truthy
 
- (fact
-  (-> (visit "http://localhost:8008")
-      (fill-in "name" "Ignacy")
-      (fill-in "password" "secret")
-      (click-button "Log in")
-      (page-has? "You are logged in")) => truthy))
+        (-> (visit "http://localhost:8008")
+            (page-has? "Unicorn")) => falsey)
+
+ (facts "about selecting element by id"
+        (-> (visit "http://localhost:8008")
+            (page-get-element-by-id "subtitle")
+            (content)) => "This is a subtitle"
+
+        (-> (visit "http://localhost:8008")
+            (page-get-element-by-id "NOT_EXISTING_ID")) => (throws ElementNotFoundException ))
+
+ (facts "about clicking links"
+        (-> (visit "http://localhost:8008")
+            (click-link "Follow me!")
+            (page-has? "I'm on a second page!")) => truthy)
+
+ (facts "about submiting forms"
+        (-> (visit "http://localhost:8008")
+            (fill-in "name" "Ignacy")
+            (fill-in "password" "secret")
+            (click-button "Log in")
+            (page-has? "You are logged in")) => truthy))
