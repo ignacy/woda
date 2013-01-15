7c5bbdd504c9ebb4b3404542c6c1ae5cc8d52812
Ignacy Moryc <imoryc@gmail.com>
2013-01-15 20:11:24 +0100

    Better doc strings for functions

--- src/woda/core.clj
+++ src/woda/core.clj
@@ -15,44 +15,59 @@
                 (-> page#
                     ~@body))))
 
-(defn visit [url]
+(defn visit
   "visit given *url* in the default browser"
+  [url]
   (.getPage browser url))
 
-(defn html-source [page]
+(defn html-source
   "returns page source as HTML"
+  [page]
   (.asXml page))
 
-(defn content [page]
+(defn content
   "get page content as text without markup"
+  [page]
   (.asText page))
 
-(defn page-has? [page string]
+(defn page-has?
   "Check if page has a string content"
+  [page string]
   (not-nil?
    (re-find (re-pattern string) (content page))))
 
-(defn get-element-by-id [page id]
+(defn get-element-by-id
   "Get HtmlElement using it's id threows ElementNotFoundException if id is not found"
+  [page id]
   (.getHtmlElementById page id))
 
-(defn get-element-by-name [page name]
+(defn get-element-by-name
+  "Find element by name attribute"
+  [page name]
   (.getElementByName page name))
 
-(defn get-element-by-css [page css]
+(defn get-element-by-css
   "Get element (list of matching elements) from page using CSS querya"
+  [page css]
   (seq (.querySelectorAll page css)))
 
-(defn execute-javascript [page javascript]
+(defn execute-javascript
   "Executes passed JavaScript code in the context of the page"
+  [page javascript]
   (.executeJavaScript page javascript)
   page)
 
-(defn fill-in [page input-name value]
+(defn fill-in
+  "Fill in *input-name* with given *value*"
+  [page input-name value]
   (.setValueAttribute (get-element-by-name page input-name) value))
 
-(defn click-button [page button-text]
+(defn click-button
+  "Click on a button with *button-text*"
+  [page button-text]
   (.click (.getInputByName (first (.getForms page)) button-text)))
 
-(defn click-link [page text]
+(defn click-link
+  "Click link with *text*"
+  [page text]
   (.click (.getAnchorByText page text)))
